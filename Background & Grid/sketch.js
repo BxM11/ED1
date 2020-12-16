42b162eef3303357a81878de7b31e3325518da0e
BxM11 <stamfordexpress1@gmail.com>
2020-12-16 13:04:02 -0500

    Updating site with grid to have pictures

--- Background & Grid/sketch.js
+++ Background & Grid/sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-  var height = 1080;
+  var height = 2080;
   createCanvas(windowWidth, height);
   stroke(0);
   var rectheight = 100;
@@ -11,29 +11,30 @@ function draw() {
   background(220);
   var gradient = 200;
   strokeWeight((height-gradient)*220/(height));
-  
+
   width = windowWidth;
   var squares = 10;
-  var buffer = 30;
-  var squareSize = 20;
-  var space = ((width-2*buffer)/squares-squareSize)/2;
+  var buffer = 20;
+  var space = 10;
+  var squareSize = (width-2*buffer)/squares-2*space;
 
-  
-  
-  
+
+
+
+
   for (var yval = 0; yval<height; yval += (height-gradient)/height){
     stroke(220-(yval)/(height-gradient)*220);
     line(0,gradient+yval,
         windowWidth, gradient+yval);
-    
+
   }
-  
+
   for (var x = buffer; x<windowWidth-2*buffer; x += (windowWidth-2*buffer)/squares ){
     for(var y = buffer; y<height-2*buffer; y += (windowWidth-2*buffer)/squares){
       strokeWeight(1);
-      fill(255,
-           y*255/(height-(squareSize+2*space)),
-           y*255/(height-(squareSize+2*space)));
+      fill(y*255/(height-(squareSize+2*space)),
+           0,
+           0);
       quad(x+space,
            y+space,
           x+space+squareSize,
@@ -42,7 +43,7 @@ function draw() {
            y+space+squareSize,
           x+space,
           y+space+squareSize);
-      
+
     }
   }
   noLoop();
